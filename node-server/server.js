da36fb364eff023b3af5ba940d86a04a93f14e0d
Deniss <denmantm@inbox.lv>
2017-12-04 19:13:57 +0000

    changing port

--- node-server/server.js
+++ node-server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var  port = process.env.PORT || 8808
+var  port = process.env.PORT || 80
 
 var app = express();
 
